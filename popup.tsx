3bd8d4936f0f2168ca139cace1fb0b1916a72998
Shivraj-K09 <sdkadgond9+github@gmail.com>
2023-11-06 20:45:17 +0530

    chat extension fronted

--- popup.tsx
+++ popup.tsx
@@ -1,29 +1,63 @@
-import { useState } from "react"
+"use client"
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+import { useChat } from "ai/react"
+import { SendHorizontal } from "lucide-react"
+
+import { Button } from "~components/ui/Button"
+import { Input } from "~components/ui/Input"
+
+import "~style.css"
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat"
+  })
+
+  const hasMessages = messages.length > 0
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="container w-[500px] h-[500px] pt-8 flex flex-col bg-black text-white">
+      <h1 className="text-4xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-cyan-500 mb-3 animate-gradientAnimation">
+        Chatbot Extension
+      </h1>
+      <div
+        className={`message-container overflow-y-auto max-h-[320px] ${
+          hasMessages
+            ? "border-2 rounded-md border-dotted border-gray-600/70"
+            : ""
+        } px-6 py-4`}>
+        {hasMessages ? (
+          messages.map((m) => (
+            <div
+              key={m.id}
+              className="text-base border-b border-gray-800 mb-2 pb-2">
+              {m.role === "user" ? "User: " : "Chatbot: "}
+              {m.content}
+            </div>
+          ))
+        ) : (
+          <div className="text-center text-xl">Talk to Chatbot</div>
+        )}
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="container fixed bottom-4 left-0 right-0">
+        <div className="flex gap-4">
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            className=""
+            placeholder="Enter Message"
+          />
+          <Button
+            variant="secondary"
+            type="submit"
+            className="shining-button text-white bg-gradient-to-r from-indigo-500 to-cyan-500">
+            <SendHorizontal className="w-6 h-6" />
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
-
-export default IndexPopup
